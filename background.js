27c2075d214928972f628a95d289624d293088a5
Chi Cong <chicong422002@gmail.com>
2023-11-11 13:18:48 +0700

    add script when creating tab

--- background.js
+++ background.js
@@ -1,10 +1,32 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+const getVidId = async () => {
+  const tab = await getCurrentTab();
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParams = tab.url.split("?")[1];
     const urlParams = new URLSearchParams(queryParams);
+    return urlParams.get("v");
+  }
+  return null;
+}
 
-    if (urlParams) {
-      chrome.tabs.sendMessage(tabId, { videoId: urlParams.get("v") });
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  if (changeInfo.status === "complete") {
+    const vidId = await getVidId();
+    if (vidId) {
+      await chrome.tabs.sendMessage(tabId, { videoId: vidId })
     }
   }
 });
+
+chrome.tabs.onCreated.addListener(async (tab) => {
+  console.log('create tab');
+  const vidId = await getVidId();
+  if (vidId) {
+    await chrome.tabs.sendMessage(tab.id, { videoId: vidId })
+  }
+})
